b8485d06c789d30610811fbe54f911407bf4783b
Alice Marique <mariquealice83@gmail.com>
2024-01-16 16:54:06 +0100

    storage autocomplete

--- assets/script.js
+++ assets/script.js
@@ -1,18 +1,13 @@
-document.getElementById('ville__button').addEventListener('click', async function (event) {
-    event.preventDefault();
-    const ville = document.getElementById('ville__adresse').value;
 
-    try {
-        const donneesMeteo = await obtenirDonneesMeteo(ville);
-        afficherPrevisionsMeteo(donneesMeteo, ville);
-    } catch (erreur) {
-        console.error('Erreur lors de la récupération des informations météorologiques :', erreur);
-    }
-});
+const villeAdresse = document.getElementById('ville__adresse');
+const villeButton = document.getElementById('ville__button');
+const previsionsMeteo = document.getElementById('previsionsMeteo');
+const villeComplete = document.getElementById('ville__complete');
+
 
 async function obtenirDonneesMeteo(ville) {
     const cleApi = 'bf1a733eb58df0e14bb400e330ad9d9a';
-    const urlApi = `https://api.openweathermap.org/data/2.5/forecast?q=${ville}&appid=${cleApi}`;
+    const urlApi = `https://api.openweathermap.org/data/2.5/forecast?q=${ville}&appid=${cleApi}&units=metric`;
 
     const reponse = await fetch(urlApi);
 
@@ -36,8 +31,8 @@ function afficherPrevisionsMeteo(donnees, ville) {
     const datesAffichees = new Set();
 
     for (const previsionsJour of donnees.list) {
-        const datePrevision = new Date(previsionsJour.dt_txt).toLocaleDateString('fr-FR');
-        const temperatureCelsius = previsionsJour.main.temp - 273.15;
+        const datePrevision = new Date(previsionsJour.dt * 1000).toLocaleDateString('fr-FR');
+        const temperatureCelsius = previsionsJour.main.temp;
 
         if (!datesAffichees.has(datePrevision)) {
             const jourElement = document.createElement('div');
@@ -45,16 +40,103 @@ function afficherPrevisionsMeteo(donnees, ville) {
 
             jourElement.innerHTML =
                 `
-                <h2> ${ville} le ${datePrevision}</h2>
+                <h2> ${ville} </h2>
+                <h3>le ${datePrevision}</h3>
                 <p>Conditions météorologiques : ${previsionsJour.weather[0].description}</p>
-                <p>Température : ${temperatureCelsius.toFixed(2)} °C</p>
+                <p>Température : ${temperatureCelsius.toFixed(0)} °C</p>
                 <p>Humidité : ${previsionsJour.main.humidity}%</p>
                 <p>Vitesse du vent : ${previsionsJour.wind.speed} m/s</p>
                 `;
 
             previsionsMeteo.appendChild(jourElement);
             datesAffichees.add(datePrevision);
+
+            if (datesAffichees.size >= 5) {
+                break
+            }
         }
+    }
+}
+
+async function entreeVille(cityName) {
+    const params = new URLSearchParams({
+        q: cityName,
+        format: "json",
+        limit: 5,
+    });
+
+    const url = `https://nominatim.openstreetmap.org/search?${params}`;
+
+    try {
+        const response = await fetch(url);
+        const data = await response.json();
+
+        const datalist = document.getElementById('ville__complete');
+        datalist.innerHTML = '';
+
+        data.forEach((result) => {
+            const option = document.createElement('option');
+            option.value = result.display_name;
+            datalist.appendChild(option);
+        });
+    } catch (error) {
+        console.error('Erreur lors de la recherche d\'autocomplétion :', error);
+    }
+}
+
+let gererChargeKeyup = null;
+
+document.getElementById('ville__adresse').addEventListener('keyup', (e) => {
+    const input = e.target;
+
+    if (input.value.length < 3) {
+        return;
+    }
+
+    if (gererChargeKeyup !== null) {
+        clearTimeout(gererChargeKeyup);
+    }
+
+    gererChargeKeyup = setTimeout(() => {
+        entreeVille(input.value);
+        gererChargeKeyup = null;
+    }, 400);
+});
+
+
+document.getElementById('ville__button').addEventListener('click', async function (event) {
+    event.preventDefault();
+
+    const ville = villeAdresse.value.trim();
+    if (!ville) {
+        alert("Veuillez entrer une ville valide.");
+        return;
+    }
+
+    try {
+        const donneesMeteo = await obtenirDonneesMeteo(ville);
+        afficherPrevisionsMeteo(donneesMeteo, ville);
+
+        
+        ajouterVilleLocalStorage(ville);
+    } catch (erreur) {
+        alert(`Erreur lors de la récupération des informations météorologiques : ${erreur}`);
+    }
+    villeAdresse.value = '';
+});
+
+
+function ajouterVilleLocalStorage(ville) {
+
+    if (typeof(Storage) !== "undefined") {
+       
+        const villesEnregistrees = JSON.parse(localStorage.getItem('villes')) || [];
+
+        villesEnregistrees.push(ville);
+
+        localStorage.setItem('villes', JSON.stringify(villesEnregistrees));
+    } else {
+        console.error("LocalStorage n'est pas pris en charge dans ce navigateur.");
     }
 }
 
